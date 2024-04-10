4de3fd969188043f6653d89f77d59ec9834dcec3
pirsonxyz <pirsonbethancourt039@gmail.com>
2024-04-10 07:41:43 -0400

    views bug fix

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -91,7 +91,7 @@ export default async function ProjectsPage() {
           <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
             {[top2, top3].map((project) => (
               <Card key={project.slug}>
-                <Article project={project} views={views[project.slug] ?? 0} />
+                <Article project={project} />
               </Card>
             ))}
           </div>
@@ -104,7 +104,7 @@ export default async function ProjectsPage() {
               .filter((_, i) => i % 3 === 0)
               .map((project) => (
                 <Card key={project.slug}>
-                  <Article project={project} views={views[project.slug] ?? 0} />
+                  <Article project={project} />
                 </Card>
               ))}
           </div>
@@ -113,7 +113,7 @@ export default async function ProjectsPage() {
               .filter((_, i) => i % 3 === 1)
               .map((project) => (
                 <Card key={project.slug}>
-                  <Article project={project} views={views[project.slug] ?? 0} />
+                  <Article project={project} />
                 </Card>
               ))}
           </div>
@@ -122,7 +122,7 @@ export default async function ProjectsPage() {
               .filter((_, i) => i % 3 === 2)
               .map((project) => (
                 <Card key={project.slug}>
-                  <Article project={project} views={views[project.slug] ?? 0} />
+                  <Article project={project} />
                 </Card>
               ))}
           </div>
