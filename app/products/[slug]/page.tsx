710ef1be3af1c51d6d374240ae6976fee113e46d
pirsonxyz <pirsonbethancourt039@gmail.com>
2024-04-09 22:57:27 -0400

    no se

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -33,7 +33,7 @@ export default async function PostPage({ params }: Props) {
   }
 
   const views =
-    (await redis.get<number>(["pageviews", "products", slug].join(":"))) ?? 0;
+    (await redis.get<number>(["pageviews", "https://emprendimientosjt.vercel.app/products", slug].join(":"))) ?? 0;
 
   return (
     <div className="bg-zinc-50 min-h-screen">
