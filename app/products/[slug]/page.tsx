80ca34cecdc391053eda24c36088e3164dacb01e
pirsonxyz <pirsonbethancourt039@gmail.com>
2024-04-09 23:05:31 -0400

    rm view error

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -32,12 +32,12 @@ export default async function PostPage({ params }: Props) {
     notFound();
   }
 
-  const views =
-    (await redis.get<number>(["pageviews", "products", slug].join(":"))) ?? 0;
+  //const views =
+  //(await redis.get<number>(["pageviews", "products", slug].join(":"))) ?? 0;
 
   return (
     <div className="bg-zinc-50 min-h-screen">
-      <Header project={project} views={views} />
+
       <ReportView slug={project.slug} />
 
       <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
