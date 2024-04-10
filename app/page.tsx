ed6ee756fcea9d74f746c70351f5bd7cbb3206e6
pirsonxyz <pirsonbethancourt039@gmail.com>
2024-04-09 21:07:59 -0400

    arreglar error 404 en nav

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "Productos", href: "/products" },
-  { name: "Contacto", href: "/contact" },
+  { name: "Productos", href: "https://emprendimientosjt.vercel.app/" },
+  { name: "Contacto", href: "https://emprendimientosjt.vercel.app/" },
 ];
 
 export default function Home() {
